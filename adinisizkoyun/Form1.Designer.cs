86feb868936ab50d73f613e622395a548f78e403
kevrrana <145452208+kevrrana@users.noreply.github.com>
2023-10-02 15:24:48 +0300

    Add files via upload

---
+++ adinisizkoyun/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace adinisizkoyun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAlisFiyati = new System.Windows.Forms.TextBox();
+            this.txtSatisFiyati = new System.Windows.Forms.TextBox();
+            this.btnKarHesaplama = new System.Windows.Forms.Button();
+            this.lblToplamKar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(82, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Alış Fiyatı: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(70, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Satış Fiyatı:";
+            // 
+            // txtAlisFiyati
+            // 
+            this.txtAlisFiyati.Location = new System.Drawing.Point(190, 88);
+            this.txtAlisFiyati.Multiline = true;
+            this.txtAlisFiyati.Name = "txtAlisFiyati";
+            this.txtAlisFiyati.Size = new System.Drawing.Size(148, 28);
+            this.txtAlisFiyati.TabIndex = 2;
+            // 
+            // txtSatisFiyati
+            // 
+            this.txtSatisFiyati.Location = new System.Drawing.Point(190, 123);
+            this.txtSatisFiyati.Multiline = true;
+            this.txtSatisFiyati.Name = "txtSatisFiyati";
+            this.txtSatisFiyati.Size = new System.Drawing.Size(148, 28);
+            this.txtSatisFiyati.TabIndex = 3;
+            // 
+            // btnKarHesaplama
+            // 
+            this.btnKarHesaplama.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKarHesaplama.Location = new System.Drawing.Point(190, 157);
+            this.btnKarHesaplama.Name = "btnKarHesaplama";
+            this.btnKarHesaplama.Size = new System.Drawing.Size(148, 37);
+            this.btnKarHesaplama.TabIndex = 4;
+            this.btnKarHesaplama.Text = "Kar Hesaplama";
+            this.btnKarHesaplama.UseVisualStyleBackColor = true;
+            this.btnKarHesaplama.Click += new System.EventHandler(this.btnKarHesaplama_Click);
+            // 
+            // lblToplamKar
+            // 
+            this.lblToplamKar.AutoSize = true;
+            this.lblToplamKar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblToplamKar.Location = new System.Drawing.Point(186, 211);
+            this.lblToplamKar.Name = "lblToplamKar";
+            this.lblToplamKar.Size = new System.Drawing.Size(104, 20);
+            this.lblToplamKar.TabIndex = 5;
+            this.lblToplamKar.Text = "Toplam Kar:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblToplamKar);
+            this.Controls.Add(this.btnKarHesaplama);
+            this.Controls.Add(this.txtSatisFiyati);
+            this.Controls.Add(this.txtAlisFiyati);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAlisFiyati;
+        private System.Windows.Forms.TextBox txtSatisFiyati;
+        private System.Windows.Forms.Button btnKarHesaplama;
+        private System.Windows.Forms.Label lblToplamKar;
+    }
+}
+
---
+++ adinisizkoyun/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adinisizkoyun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKarHesaplama_Click(object sender, EventArgs e)
+        {
+            double satisfiyati, alisfiyati, toplamkar;
+            alisfiyati = Convert.ToDouble(txtAlisFiyati.Text);
+            satisfiyati = Convert.ToDouble(txtSatisFiyati.Text);
+
+            toplamkar = satisfiyati - alisfiyati;
+            lblToplamKar.Text = "Toplam Kar: " + toplamkar;
+        }
+    }
+}
